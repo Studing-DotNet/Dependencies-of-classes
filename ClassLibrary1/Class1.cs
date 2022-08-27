2ac7012e669a52be85a4b2eae49813b96fb82d47
lexipun <oleg.zolotar.develop@gmail.com>
2022-08-27 15:19:07 +0300

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class A
+    {
+
+        public virtual void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+
+    }
+
+    public class B : A
+    {
+        private int field;
+        protected int field2;
+        public int Property { get; set; }
+
+
+        public B()
+        {
+
+        }
+
+
+        public override void SayHello()
+        {
+            Console.WriteLine("World");
+        }
+    }
+
+    public class C : A
+    {
+        public override void SayHello()
+        {
+            base.SayHello();
+            Console.WriteLine("World");
+        }
+    }
+
+    public class D : A
+    {
+        public  void SayHello()
+        {
+            Console.WriteLine("World");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
